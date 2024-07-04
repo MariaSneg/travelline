aa2a4467622331663ef0ead9e833b9d5d28daba6
MariaSneg <mashakotik2090@gmail.com>
2024-07-04 13:44:56 +0300

    Add Dictionary

---
+++ Dictionary/Dictionary/CDictionary.cs
@@ -0,0 +1,69 @@
+﻿class CDictionary
+{
+    private Dictionary<string, string> _dict = new Dictionary<string, string>();
+    private bool _isReverse;
+    private bool _isChanged = false;
+
+    public CDictionary( bool IsReverse ) 
+    {
+        _isReverse = IsReverse;
+    }
+
+   public void ParseDictionary(string NameFile)
+    {
+        StreamReader file = new StreamReader(NameFile);
+        string line = file.ReadLine();
+        while (line != null)
+        {
+            string[] parts = line.Split( ':' );
+            if (_isReverse)
+            {
+                _dict[ parts[ 1 ] ] = parts[ 0 ];
+            }
+            else
+            {
+                _dict[ parts[ 0 ] ] = parts[ 1 ];
+            }
+            line = file.ReadLine();
+        }
+    }
+
+    public string TranslateWord(string word)
+    {
+        if(_dict.TryGetValue(word, out string translation))
+        {
+            return translation; 
+        }
+        return "";
+    }
+
+    public void AddNewWord(string word, string translation )
+    {
+        if (!string.IsNullOrEmpty(translation) && !string.IsNullOrEmpty( translation ) )
+        {
+            if (_isReverse)
+            {
+                _dict[ translation ] = word; 
+                return;
+            }
+            _dict[ word ] = translation;
+            _isChanged = true;
+        }
+        else
+        {
+            Console.WriteLine( "Слово проигнорировано" );
+        }
+    }
+
+    public void SaveChanges(string NameFile)
+    {
+        if(_isChanged)
+        {
+            StreamWriter file = new StreamWriter(NameFile);
+            foreach (var translate in _dict )
+            {
+                file.WriteLine($"{translate.Key}:{translate.Value}");
+            }
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,50 @@
+﻿CDictionary engToRusDict = new CDictionary( false );
+CDictionary rusToEngDict = new CDictionary( true );
+
+string fileName = "C:\\Programming\\travalline\\Dictionary\\Dictionary\\dict.txt";
+engToRusDict.ParseDictionary( fileName );
+rusToEngDict.ParseDictionary( fileName );
+
+Console.WriteLine( "Для выхода введите '...'" );
+string word;
+while ( true )
+{
+    word = Console.ReadLine();
+    if ( word == "..." )
+    {
+        engToRusDict.SaveChanges( fileName );
+        break;
+    }
+
+    string translation = engToRusDict.TranslateWord( word );
+    if ( translation == "")
+    {
+        translation = rusToEngDict.TranslateWord( word );
+        if( translation == "")
+        {
+            NewWord();
+            continue;
+        }
+    }
+    Console.WriteLine( translation );
+}
+
+void NewWord()
+{
+    Console.WriteLine( "Неизвестное слово. Добавить его в словарь? Для согласия введите Y или y" );
+    string answer = Console.ReadLine();
+    if ( answer == "y")
+    {
+        Console.WriteLine( "Введите новое слово на английском" );
+        string newWord = Console.ReadLine();
+        Console.WriteLine("Введите слово на русском");
+        string newTranslation = Console.ReadLine();
+        engToRusDict.AddNewWord( newWord, newTranslation );
+        rusToEngDict.AddNewWord( newWord, newTranslation );
+        Console.WriteLine( "Слово добавлено в словарь" );
+    }
+    else
+    {
+        Console.WriteLine( "Слово проигнорировано" );
+    }
+}
