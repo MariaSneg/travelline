b37809c5d5d1c9650bd29ab9485a30da0d31fe03
MariaSneg <mashakotik2090@gmail.com>
2024-07-15 23:44:44 +0300

    add Debug

---
+++ Accomodations/Accommodations/AccommodationsProcessor.cs
@@ -0,0 +1,136 @@
+using System.Globalization;
+using Accommodations.Commands;
+using Accommodations.Dto;
+
+namespace Accommodations;
+
+public static class AccommodationsProcessor
+{
+    private static BookingService _bookingService = new();
+    private static Dictionary<int, ICommand> _executedCommands = new();
+    private static int s_commandIndex = 0;
+
+    public static void Run()
+    {
+        Console.WriteLine( "Booking Command Line Interface" );
+        Console.WriteLine( "Commands:" );
+        Console.WriteLine( "'book <UserId> <Category> <StartDate> <EndDate> <Currency>' - to book a room" );
+        Console.WriteLine( "'cancel <BookingId>' - to cancel a booking" );
+        Console.WriteLine( "'undo' - to undo the last command" );
+        Console.WriteLine( "'find <BookingId>' - to find a booking by ID" );
+        Console.WriteLine( "'search <StartDate> <EndDate> <CategoryName>' - to search bookings" );
+        Console.WriteLine( "'exit' - to exit the application" );
+
+        string input;
+        while ( ( input = Console.ReadLine() ) != "exit" )
+        {
+            try
+            {
+                ProcessCommand( input );
+            }
+            catch ( ArgumentException ex )
+            {
+                Console.WriteLine( $"Error: {ex.Message}" );
+            }
+        }
+    }
+
+    private static void ProcessCommand( string input )
+    {
+        string[] parts = input.Split( ' ' );
+        string commandName = parts[ 0 ];
+
+        switch ( commandName )
+        {
+            case "book":
+                if ( parts.Length != 6 )
+                {
+                    Console.WriteLine( "Invalid number of arguments for booking." );
+                    return;
+                }
+
+                if ( !Enum.TryParse( parts[ 5 ], true, out CurrencyDto currency ) ) //при неправильной валюте выбрасывается исключение
+                {
+                    throw new ArgumentException( "Invalid currency" );
+                }
+
+                BookingDto bookingDto = new()
+                {
+                    UserId = int.Parse( parts[ 1 ] ),
+                    Category = parts[ 2 ],
+                    StartDate = ParseStringToDate( parts[ 3 ] ), //функция с выбрасыванием исключения
+                    EndDate = ParseStringToDate( parts[ 4 ] ),
+                    Currency = currency,
+                };
+
+                BookCommand bookCommand = new( _bookingService, bookingDto );
+                bookCommand.Execute();
+                _executedCommands.Add( ++s_commandIndex, bookCommand );
+                Console.WriteLine( "Booking command run is successful." );
+                break;
+
+            case "cancel":
+                if ( parts.Length != 2 )
+                {
+                    Console.WriteLine( "Invalid number of arguments for canceling." );
+                    return;
+                }
+
+                Guid bookingId = Guid.Parse( parts[ 1 ] );
+                CancelBookingCommand cancelCommand = new( _bookingService, bookingId );
+                cancelCommand.Execute();
+                _executedCommands.Add( ++s_commandIndex, cancelCommand );
+                Console.WriteLine( "Cancellation command run is successful." );
+                break;
+
+            case "undo":
+                if ( _executedCommands.Count == 0 )
+                {
+                    Console.WriteLine( "No commands to undo" ); //проверка на пустой список команд
+                    return;
+                }
+                _executedCommands[ s_commandIndex ].Undo();
+                _executedCommands.Remove( s_commandIndex );
+                s_commandIndex--;
+                Console.WriteLine( "Last command undone." );
+
+                break;
+            case "find":
+                if ( parts.Length != 2 )
+                {
+                    Console.WriteLine( "Invalid arguments for 'find'. Expected format: 'find <BookingId>'" );
+                    return;
+                }
+                Guid id = Guid.Parse( parts[ 1 ] );
+                FindBookingByIdCommand findCommand = new( _bookingService, id );
+                findCommand.Execute();
+                break;
+
+            case "search":
+                if ( parts.Length != 4 )
+                {
+                    Console.WriteLine( "Invalid arguments for 'search'. Expected format: 'search <StartDate> <EndDate> <CategoryName>'" );
+                    return;
+                }
+                DateTime startDate = ParseStringToDate( parts[ 1 ] ); //функция с выбрасыванием исключения
+                DateTime endDate = ParseStringToDate( parts[ 2 ] );
+                string categoryName = parts[ 3 ];
+                SearchBookingsCommand searchCommand = new( _bookingService, startDate, endDate, categoryName );
+                searchCommand.Execute();
+                break;
+
+            default:
+                Console.WriteLine( "Unknown command." );
+                break;
+        }
+    }
+
+    private static DateTime ParseStringToDate( string str ) //функция для перевода строки в дату
+    {
+        if ( !DateTime.TryParse( str, out DateTime date ) )
+        {
+            throw new ArgumentException( "Invalid date" );
+        }
+        return date;
+    }
+}
